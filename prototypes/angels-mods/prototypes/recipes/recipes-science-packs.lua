be70516dfa70966316b330af74140884db030bf7
Fabian Maurer <dark.shadow4@web.de>
2020-06-03 18:14:54 +0200

    Only enable exploration science pack fix when Angel's Exploration is installed

--- prototypes/angels-mods/prototypes/recipes/recipes-science-packs.lua
+++ prototypes/angels-mods/prototypes/recipes/recipes-science-packs.lua
@@ -1,23 +1,26 @@
 
+if mods["angelsexploration"] then
 
-RECIPE {
-    type = "recipe",
-    name = "exploration-science-pack",
-    enabled = true,
-    energy_required = 10,
-    ingredients = {
-        {type = "item", name = "flask", amount = 4},
-		{type = "item", name = "tin-plate", amount = 4},
-		{type = "item", name = "lead-plate", amount = 4},
-		{type = "item", name = "temperate-3", amount = 4},
-		--maybe use cellulose fiber?
-		{type = "item", name = "cellulose", amount = 4}
-    },
-    results = {
-        {type = "item", name = "exploration-science-pack", amount = 6}
-    },
-    icon = "__pycoalprocessinggraphics__/graphics/icons/science-pack-1.png",
-    icon_size = 32,
-    subgroup = "py-items",
-    order = "a1"
-}
+	RECIPE {
+		type = "recipe",
+		name = "exploration-science-pack",
+		enabled = true,
+		energy_required = 10,
+		ingredients = {
+			{type = "item", name = "flask", amount = 4},
+			{type = "item", name = "tin-plate", amount = 4},
+			{type = "item", name = "lead-plate", amount = 4},
+			{type = "item", name = "temperate-3", amount = 4},
+			--maybe use cellulose fiber?
+			{type = "item", name = "cellulose", amount = 4}
+		},
+		results = {
+			{type = "item", name = "exploration-science-pack", amount = 6}
+		},
+		icon = "__pycoalprocessinggraphics__/graphics/icons/science-pack-1.png",
+		icon_size = 32,
+		subgroup = "py-items",
+		order = "a1"
+	}
+
+end
