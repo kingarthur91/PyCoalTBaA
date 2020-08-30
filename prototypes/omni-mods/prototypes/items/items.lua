0c0659ec11dc22fc50a1c1bc3aa464e76cc1546e
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-08-30 06:01:45 -0400

    adding omni pack

---
+++ prototypes/omni-mods/prototypes/items/items.lua
@@ -0,0 +1,13 @@
+
+ITEM {
+    type = "tool",
+    name = "omni-science-pack",
+    icon = "__base__/graphics/icons/production-science-pack.png",
+    icon_size = 64,
+    subgroup = "py-alienlife-items",
+    order = "a-a",
+    stack_size = 200,
+    durability = 1,
+    durability_description_key = "description.science-pack-remaining-amount-key",
+    durability_description_value = "description.science-pack-remaining-amount-value"
+}
--- prototypes/omni-mods/prototypes/recipes/recipes.lua
+++ prototypes/omni-mods/prototypes/recipes/recipes.lua
@@ -2,46 +2,32 @@ local fun = require("functions/functions")
 
 RECIPE {
     type = "recipe",
-    name = "log0",
-    category = "fwf-basic",
-    enabled = true,
-    energy_required = 60,
-    ingredients = {},
-    results = {
-        {type = "item", name = "log", amount = 3}
-    },
-    icon = "__pycoalprocessinggraphics__/graphics/icons/log.png",
-    icon_size = 32,
-    subgroup = "py-items",
-    order = "c6"
-}
-RECIPE {
-    type = "recipe",
-    name = "wood0",
-    category = "burner-wpu",
-    enabled = true,
-    energy_required = 15,
-    ingredients = {
-		{type = "item", name = "log", amount = 1}
-	},
-    results = {
-        {type = "item", name = "wood", amount = 2}
-    },
-    icon = "__base__/graphics/icons/wood.png",
-    icon_size = 64,
-    subgroup = "py-items",
-    order = "c6"
-}
-RECIPE {
-    type = "recipe",
     name = "latex0",
     category = "pulp-basic",
     enabled = true,
     energy_required = 10,
     ingredients = {
-        {type = 'item', name = 'latex-slab', amount = 1},
+        {type = 'item', name = 'latex-slab', amount = 2},
     },
     results = {
         {type = 'item', name = 'latex', amount = 1}
     },
 }:change_category('pulp')
+
+RECIPE {
+    type = 'recipe',
+    name = 'omni-science-pack',
+    category = 'crafting',
+    enabled = true,
+    energy_required = 2,
+    ingredients = {
+        {type = 'item', name = 'omniplate', amount = 2},
+        {type = 'item', name = 'moss', amount = 3},
+        {type = 'item', name = 'iron-ore', amount = 2},
+        {type = 'item', name = 'copper-ore', amount = 2},
+        {type = 'item', name = 'omnite-brick', amount = 1}
+    },
+    results = {
+        {type = 'item', name = 'omni-science-pack', amount = 4},
+    },
+}
