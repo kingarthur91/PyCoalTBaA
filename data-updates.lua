eb9f677040c7fd5e8cb9fc07dc50b59c0776039f
kingarthur91 <arthurkoy_durbin@yahoo.com>
2020-12-19 03:00:31 -0500

    fixing circuit issues

--- data-updates.lua
+++ data-updates.lua
@@ -2,3 +2,6 @@
 
 --angel mods
 require('prototypes/angels-mods/Data-Updates')
+
+--bob mods
+require('prototypes/bobs-mods/Data-Updates')
--- prototypes/bobs-mods/prototypes/overrides/overrides-updates.lua
+++ prototypes/bobs-mods/prototypes/overrides/overrides-updates.lua
@@ -1,4 +1,5 @@
 
 if mods['bobelectronics'] and mods['pyhightech'] then
+    fun.global_item_replacer('basic-circuit-board', 'pcb1')
     data.raw.recipe['basic-circuit-board'].enabled = false
 end
