c859c41809b336ec52cbb8640efca7b83c05b02f
Erkigmo <122825731+Erkigmo@users.noreply.github.com>
2023-04-08 11:40:31 -0400

    Update overrides-updates.lua

--- prototypes/bobs-mods/prototypes/overrides/overrides-updates.lua
+++ prototypes/bobs-mods/prototypes/overrides/overrides-updates.lua
@@ -7,3 +7,9 @@ end
 if mods['boblogistics'] and settings.startup['bobmods-logistics-inserteroverhaul'].value == true then
     fun.ingredient_replace('rare-earth-mine','fast-inserter','red-inserter')
 end
+
+if settings.startup["bobmods-power-burnergenerator"].value == true then
+   if mods['bobpower'] and ['pyalienlife'] then
+      fun.ingredient_replace('collector','steam-engine','bob-burner-generator')
+   end
+end
