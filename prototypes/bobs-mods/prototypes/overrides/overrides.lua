fe130028c37882551d63dc8486fd1521c298ec9d
Erkigmo <122825731+Erkigmo@users.noreply.github.com>
2023-04-08 11:38:13 -0400

    Update overrides.lua

--- prototypes/bobs-mods/prototypes/overrides/overrides.lua
+++ prototypes/bobs-mods/prototypes/overrides/overrides.lua
@@ -2,3 +2,9 @@
 if mods['boblogistics'] then
    fun.ingredient_replace('borax-mine','transport-belt','basic-transport-belt')
 end
+
+if settings.startup["bobmods-power-burnergenerator"].value == true then
+   if mods['bobpower'] and ['pyalienlife'] then
+      fun.ingredient_replace('collector','steam-engine','bob-burner-generator')
+   end
+end
