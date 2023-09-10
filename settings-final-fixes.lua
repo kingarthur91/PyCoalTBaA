108d10f26a78cfe1ab06e6b5a00fd12062dc5f77
Erkigmo <122825731+Erkigmo@users.noreply.github.com>
2023-09-09 20:52:37 -0400

    Fix crash if people had already played bob's mining

--- settings-final-fixes.lua
+++ settings-final-fixes.lua
@@ -3,4 +3,6 @@ if mods['angelsindustries'] then
 	--data.raw["bool-setting"]["angels-enable-components"].hidden = true
 	data.raw["bool-setting"]["angels-enable-industries"].forced_value = true
 	data.raw["bool-setting"]["angels-enable-industries"].hidden = true
+	data.raw["bool-setting"]["bobmods-mining-miningdrills"].forced_value = true
+	data.raw["bool-setting"]["bobmods-mining-miningdrills"].hidden = true
 end
