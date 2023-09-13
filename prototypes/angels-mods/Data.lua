aa89825f501f179edf0aa100c20d0a724a434214
Erkigmo <122825731+Erkigmo@users.noreply.github.com>
2023-09-13 13:34:00 -0400

    Bug fix (pt 1)

--- prototypes/angels-mods/Data.lua
+++ prototypes/angels-mods/Data.lua
@@ -4,8 +4,8 @@ require('prototypes/angels-mods/prototypes/overrides/overrides')
 require('prototypes/angels-mods/prototypes/recipes/recipes')
 
 
-if mods['angelsrefining'] then
-	if angelsmods.industries.components then
+if mods['angelsindustries'] then
+	if settings.startup["angels-enable-components"].value then
 		require('prototypes/angels-mods/prototypes/overrides/ai/overrides')
 	else
 		require('prototypes/angels-mods/prototypes/overrides/no-ai/overrides')
