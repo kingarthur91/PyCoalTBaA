bd3661a0a16619d29cccd705a635a1c8641e3054
Erkigmo <122825731+Erkigmo@users.noreply.github.com>
2023-09-30 14:03:48 -0400

    yes (#81)
    * fix crash
    
    * Add files via upload
    
    * Update changelog.txt
    
    * Update info.json

--- prototypes/bobs-mods/prototypes/recipes/recipes.lua
+++ prototypes/bobs-mods/prototypes/recipes/recipes.lua
@@ -1,14 +1,1 @@
-if mods['bobelectronics'] and mods['pyhightech'] and not mods['angelsindustries'] then
-  	RECIPE {
-    	type = "recipe",
-    	name = "primitive-circuit-board",
-    	category = "electronics",
-    	energy_required = 1,
-    	ingredients = {
-      		{ "fiberboard", 5 },
-      		{ "resin", 3 },
-      		{ "copper-cable", 10 },
-    	},
-    	result = "primitive-circuit-board",
-    }:add_unlock('coal-processing-1')
-end
+
